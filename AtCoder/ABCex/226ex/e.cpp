d68a5f936766defb2a58311563013753e7fc2b08
浅利厚紀 <asariatsushiki@asariatsushikinoMacBook-Pro.local>
2022-04-30 00:50:43 +0900

    問題を解いた

--- AtCoder/ABCex/226ex/e.cpp
+++ AtCoder/ABCex/226ex/e.cpp
@@ -25,63 +25,55 @@ ll powmod(ll x , ll n){
 }
 
 struct UnionFind {
-    vector<int> par ; //親
-    vector<int> lank ; //木の深さ
-    vector<int> volume ; //木のサイズ
-    vector<int> edge ; //辺の数
-    UnionFind(int n){
-        //n要素で初期化
-        par.resize(n) ;
-        lank.resize(n) ;
-        volume.resize(n) ;
-        edge.resize(n) ;
-        for(int i = 0 ; i < n ; i++){
-            par[i] = i ;
-            lank[i] = 0 ;
-            volume[i] = 1 ;
-            edge[i] = 0 ;
-        }
-    }
-    //木の根を求める
-    int root(int x){
-        if(par[x] == x){
-            return x ;
-        } else{
-            return par[x] = root(par[x]) ;
-        }
-    }
+    private:
+        vector<int> par ; //親
+        vector<int> lank ; //木の深さ
+        vector<int> volume ; //構成する集合のサイズ
+        vector<int> edge ; //構成する集合の辺の数
 
-    //xとyの属する集合を合併
-    void unite(int x , int y){
-        x = root(x);
-        y = root(y) ;
-        if(x == y) {
-            edge[x]++ ;
-            return ;
+    public:
+        UnionFind(int n){
+            //n要素で初期化
+            par.resize(n) ;
+            lank.resize(n) ;
+            volume.resize(n) ;
+            edge.resize(n) ;
+            for(int i = 0 ; i < n ; i++){
+                par[i] = i ;
+                lank[i] = 0 ;
+                volume[i] = 1 ;
+                edge[i] = 0 ;
+            }
         }
-        if(lank[x] < lank[y]){
-            par[x] = y ;
-            volume[y] += volume[x] ;
-            edge[y] += edge[x] + 1 ;
-        } else {
-            par[y] = x ;
-            volume[x] += volume[y] ;
-            edge[x] += edge[y] + 1 ;
-            if(lank[x] == lank[y]) lank[x]++ ;
+        //木の根を求める
+        int root(int x) {
+            if(par[x] == x) return x ;
+            else return par[x] = root(par[x]) ;
         }
-    }
 
-    bool same(int x , int y){
-        return root(x) == root(y) ;
-    }
+        //xとyの属する集合を合併
+        void unite(int x , int y){
+            x = root(x);
+            y = root(y) ;
+            if(x == y) {
+                edge[x]++ ;
+                return ;
+            }
+            if(lank[x] < lank[y]){
+                par[x] = y ;
+                volume[y] += volume[x] ;
+                edge[y] += edge[x] + 1 ;
+            } else {
+                par[y] = x ;
+                volume[x] += volume[y] ;
+                edge[x] += edge[y] + 1 ;
+                if(lank[x] == lank[y]) lank[x]++ ;
+            }
+        }
 
-    int size(int x){
-        return volume[root(x)] ;
-    }
-
-    int edge_num(int x){
-        return edge[root(x)] ;
-    }
+        bool same(int x , int y) { return root(x) == root(y) ; }
+        int size(int x) { return volume[root(x)] ; }
+        int edge_num(int x) { return edge[root(x)] ; }
 } ;
 
 int n , m ;

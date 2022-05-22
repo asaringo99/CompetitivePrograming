65e7f8f4c21927f62a9980c4591bb74b6421f1d3
浅利厚紀 <asariatsushiki@asariatsushikinoMacBook-Pro.local>
2022-05-22 14:08:30 +0900

    中央値管理を更新

--- library/データ構造/中央値管理/オンライン中央値管理.cpp
+++ library/データ構造/中央値管理/オンライン中央値管理.cpp
@@ -18,6 +18,7 @@ struct Median{
 
     private :
         multiset<T> lef , rig ;
+        T sum_magin_from_median = 0 ;
 
         void push_(T x){
             lef.insert(x) ; rig.insert(x) ;
@@ -29,6 +30,7 @@ struct Median{
             if(l > r) swap(l,r) ;
             lef.insert(l) ;
             rig.insert(r) ;
+            sum_magin_from_median += r - l ;
         }
 
         T get_(bool type){
@@ -46,32 +48,52 @@ struct Median{
         }
 
         void erase_(T x){
-            T val = x ;
-            if(lef.count(val) >= 2){
-                lef.erase(lef.find(val)) ;
-                lef.erase(lef.find(val)) ;
+            T median1 = get() ;
+            if(lef.count(x) >= 2){
+                lef.erase(lef.find(x)) ;
+                lef.erase(lef.find(x)) ;
                 auto it = rig.begin() ;
                 lef.insert(*it) ;
                 rig.erase(it) ;
             }
-            else if(rig.count(val) >= 2){
-                rig.erase(rig.find(val)) ;
-                rig.erase(rig.find(val)) ;
+            else if(rig.count(x) >= 2){
+                rig.erase(rig.find(x)) ;
+                rig.erase(rig.find(x)) ;
                 auto it = lef.end() ; it-- ;
                 rig.insert(*it) ;
                 lef.erase(it) ;
             }
             else{
-                lef.erase(lef.find(val)) ;
-                rig.erase(rig.find(val)) ;
+                lef.erase(lef.find(x)) ;
+                rig.erase(rig.find(x)) ;
             }
+            T median2 = get() ;
+            if(((lef.size()+rig.size())/2)%2==0) sum_magin_from_median -= abs(median1 - x) ;
+            if(((lef.size()+rig.size())/2)%2==1) sum_magin_from_median -= abs(median2 - x) ;
         }
     public :
         void push(T x) { return push_(x) ; }
         void erase(T x) { erase_(x) ; }
-        T get(bool type = false) { return get_(type) ;}
+        T get(bool type = false) { return get_(type) ; }
+        T sum() { return sum_magin_from_median ; }
 };
 
-// push  : 値x を挿入する
-// erase : 値x を削除する
-// get   : 中央値を取得する
+// push      : 値x を挿入する
+// erase     : 値x を削除する
+// get(type) : type = true => (右端+左端)/2の中央値を取得する, 左端の中央値を取得する
+// sum       : Σ(Ai-x) の値を得る (xは中央値s)
+
+int main(){
+    Median<ll> med ;
+    med.push(3) ;
+    med.push(5) ;
+    med.push(7) ;
+    med.push(9) ;
+    med.push(11) ;
+    med.push(14) ;
+    med.push(15) ;
+    med.erase(3) ;
+    med.erase(5) ;
+    med.erase(7) ;
+    cout << med.sum() << endl ;
+}
